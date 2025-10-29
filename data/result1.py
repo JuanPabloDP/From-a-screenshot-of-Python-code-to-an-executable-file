a80a92abc272068de29b5d2eba30b7a634a9a23e
Gonzalo Higuera <gonzalomakenly22@gmail.com>
2025-10-29 17:59:24 -0600

    added data

---
+++ data/result1.py
@@ -0,0 +1,12 @@
+
+# Simple Calculator
+	#indentacion de apoyo
+
+a=int(input('Enter the first number:'))
+b=int(input('Enter the second number:'))
+print('sum:',atb)
+
+print ('Difference:',a-b)
+print('MuLtipLle:*,a*b)
+
+print( 'Division: *,a/b)
