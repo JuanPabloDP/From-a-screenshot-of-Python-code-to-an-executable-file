769a10439520e7c38cce7fdbdadabad06845e7f2
JuanPabloDP <juanpablo.dp.glez@gmail.com>
2025-10-29 16:24:27 -0600

    commit codeScreenshot as a function

--- scripts/codeScreenshot.py
+++ scripts/codeScreenshot.py
@@ -1,52 +1,50 @@
 import cv2
 import matplotlib.pyplot as plt
-
 import pytesseract
 
 from stringToScript import string_to_script
 
-pytesseract.pytesseract.tesseract_cmd = r"source\Tesseract-OCR\tesseract.exe"
+def screenshot_to_code(screenshot,fileName):
+    pytesseract.pytesseract.tesseract_cmd = r"source\Tesseract-OCR\tesseract.exe"
 
-code = cv2.imread("data\codpy.png")
+    data = pytesseract.image_to_data(screenshot, output_type=pytesseract.Output.DATAFRAME)
 
-data = pytesseract.image_to_data(code, output_type=pytesseract.Output.DATAFRAME)
+    data = data.dropna(subset=['text'])
+    data = data[data['text'].str.strip() != '']
 
-data = data.dropna(subset=['text'])
-data = data[data['text'].str.strip() != '']
+    lines = (data
+        .sort_values(['block_num', 'par_num', 'line_num', 'left'])
+        .groupby(['page_num', 'block_num', 'par_num', 'line_num'], as_index=False)
+        .agg({
+            'left': 'min',
+            'text': lambda x: ' '.join(x)
+        })
+    )
 
-lines = (data
-    .sort_values(['block_num', 'par_num', 'line_num', 'left'])
-    .groupby(['page_num', 'block_num', 'par_num', 'line_num'], as_index=False)
-    .agg({
-        'left': 'min',
-        'text': lambda x: ' '.join(x)
-    })
-)
+    counts, bins, _ = plt.hist(lines["left"])
+    plt.close()
 
-counts, bins, _ = plt.hist(lines["left"])
-plt.close()
+    clases = []
+    for i in range(len(bins)-1):
+        if counts[i] > 0:
+            clases.append((bins[i], bins[i+1]))
 
-clases = []
-for i in range(len(bins)-1):
-    if counts[i] > 0:
-        clases.append((bins[i], bins[i+1]))
+    def obtener_indentacion(numero):
+        for i, (start, end) in enumerate(clases):
+            if start <= numero < end:
+                return i
+        return len(clases) - 1
 
-def obtener_indentacion(numero):
-    for i, (start, end) in enumerate(clases):
-        if start <= numero < end:
-            return i
-    return len(clases) - 1
+    lines["Indentacion"] = lines["left"].apply(obtener_indentacion)
 
-lines["Indentacion"] = lines["left"].apply(obtener_indentacion)
+    string = ""
 
-string = ""
+    for i in range(len(lines)):
+        if lines.iloc[i]["line_num"] == 1:
+            aux = "\n"
+        else:
+            aux = ""
+        aux += lines.iloc[i]["Indentacion"] * "\t" + lines.iloc[i]["text"] + "\n"
+        string += aux
 
-for i in range(len(lines)):
-    if lines.iloc[i]["line_num"] == 1:
-        aux = "\n"
-    else:
-        aux = ""
-    aux += lines.iloc[i]["Indentacion"] * "\t" + lines.iloc[i]["text"] + "\n"
-    string += aux
-
-string_to_script(string,"resultado",execute=True)
+    string_to_script(string,fileName,execute=True)
